65251b50ea062488b90774c52e6cde508eb69f1c
iRove108 <iRove108@users.noreply.github.com>
2018-09-28 18:29:04 -0400

    Added basic movement functionality

--- src/movement.c
+++ src/movement.c
@@ -8,3 +8,32 @@ void stop_all_motors(void)
     motorSet(wheel_RB, 0); //Right back wheel
     motorSet(wheel_LB, 0); //Left back wheel
 }
+
+void move_full_forward(void)
+{
+    motorSet(wheel_RF, 127); //Right front wheel
+    motorSet(wheel_LF, 127); //Left front wheel
+    motorSet(wheel_RB, 127); //Right back wheel
+    motorSet(wheel_LB, 127); //Left back wheel
+}
+void move_full_backward(void)
+{
+    motorSet(wheel_RF, -127); //Right front wheel
+    motorSet(wheel_LF, -127); //Left front wheel
+    motorSet(wheel_RB, -127); //Right back wheel
+    motorSet(wheel_LB, -127); //Left back wheel
+}
+void turn_full_left(void)
+{
+    motorSet(wheel_RF, 127); //Right front wheel
+    motorSet(wheel_LF, 0); //Left front wheel
+    motorSet(wheel_RB, 127); //Right back wheel
+    motorSet(wheel_LB, 0); //Left back wheel
+}
+void turn_full_right(void)
+{
+    motorSet(wheel_RF, 0); //Right front wheel
+    motorSet(wheel_LF, 127); //Left front wheel
+    motorSet(wheel_RB, 0); //Right back wheel
+    motorSet(wheel_LB, 127); //Left back wheel
+}
